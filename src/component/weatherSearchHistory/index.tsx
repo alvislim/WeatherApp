6d02a0e9d89c87e1eaa6b40efe4f45d0cffae72a
alvislim <luvhatehero1@gmail.com>
2024-03-12 16:11:22 +0800

    removed reverse order in weatherarr for search history

--- src/component/weatherSearchHistory/index.tsx
+++ src/component/weatherSearchHistory/index.tsx
@@ -13,29 +13,27 @@ const WeatherSearchHistory = (
   const { onClickSearch, onClickDelete, weatherArr } = props;
 
   return (
-    <div className="weather-list-wrapper">
-      <div className="header"> Search History</div>
+    <div className='weather-list-wrapper'>
+      <div className='header'> Search History</div>
       {weatherArr.length === 0 && (
-        <div className="no-history">No past search history</div>
+        <div className='no-history'>No past search history</div>
       )}
-      {weatherArr.reverse().map((info) => (
-        <span className="weather-detail-wrapper">
-          <div className="loca-timestamp-wrapper">
-            <p className="location">{`${info.country}, ${info.countryCode}`}</p>
-            <p className="timestamp">{info.dateTimeStamp}</p>
+      {weatherArr.map((info) => (
+        <span className='weather-detail-wrapper'>
+          <div className='loca-timestamp-wrapper'>
+            <p className='location'>{`${info.country}, ${info.countryCode}`}</p>
+            <p className='timestamp'>{info.dateTimeStamp}</p>
           </div>
-          <span className="ico-wrapper">
+          <span className='ico-wrapper'>
             <div
-              className="circle-ico-wrapper"
-              onClick={() => onClickSearch(info.country || "")}
-            >
-              <div className="ico-search-dark" />
+              className='circle-ico-wrapper'
+              onClick={() => onClickSearch(info.country || "")}>
+              <div className='ico-search-dark' />
             </div>
             <div
-              className="circle-ico-wrapper"
-              onClick={() => onClickDelete(info.id)}
-            >
-              <div className="ico-delete-dark" />
+              className='circle-ico-wrapper'
+              onClick={() => onClickDelete(info.id)}>
+              <div className='ico-delete-dark' />
             </div>
           </span>
         </span>
